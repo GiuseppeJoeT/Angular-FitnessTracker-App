25be51b6fe446a7c12fc934d620b9fe11f71b294
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-12-19 11:54:39 +0100

    Two way binding

--- NG-Fitness-Tracker-App/src/app/products.component.ts
+++ NG-Fitness-Tracker-App/src/app/products.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-products',
-    templateUrl: './products.component.html'
+    templateUrl: './products.component.html',
+    styleUrls: ['./app.component.css']
 })
 
 export class ProductsComponent {
     productName = 'A book';
+    isDisabled = true;
+
+    constructor() {
+        setTimeout(() => {
+            // this.productName = 'A Tree';
+            // Property binding
+            this.isDisabled = false;
+        }, 3000);
+    }
 }
