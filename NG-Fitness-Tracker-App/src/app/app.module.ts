ae5e255db860ec7d95a26abc46296379715e2bd9
Giuseppe Tiberi <tiberiweb@gmail.com>
2018-12-19 11:55:13 +0100

    FormsModule import

--- NG-Fitness-Tracker-App/src/app/app.module.ts
+++ NG-Fitness-Tracker-App/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductsComponent } from './products.component';
@@ -12,7 +13,8 @@ import { ProductComponent } from './product/product.component';
     ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
